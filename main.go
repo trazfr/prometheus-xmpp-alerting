da1d1eaeceb0f04b67471e6837d6746226764560
Alexandre Blazart <alexandre@blazart.fr>
2025-10-09 22:25:58 +0200

    listen to unix socket if the address is an absolute path

--- main.go
+++ main.go
@@ -3,11 +3,38 @@ package main
 import (
 	"fmt"
 	"log/slog"
+	"net"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+func getAddr(addr string) string {
+	if addr == "" {
+		return ":http"
+	}
+	return addr
+}
+
+func getNetwork(addr string) string {
+	if strings.HasPrefix(addr, "/") {
+		return "unix"
+	}
+	return "tcp"
+}
+
+func listenAndServe(s *http.Server) error {
+	network := getNetwork(s.Addr)
+	addr := getAddr(s.Addr)
+	slog.Info("Create HTTP server", "addr", addr, "network", network)
+	ln, err := net.Listen(network, addr)
+	if err != nil {
+		return err
+	}
+	return s.Serve(ln)
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -41,5 +68,5 @@ func main() {
 	mux.Handle("/alert", NewAlertHandler(config, xmpp))
 	mux.Handle("/send", NewSendHandler(xmpp))
 	mux.Handle("/metrics", promhttp.Handler())
-	slog.Info("Server closed", "error", server.ListenAndServe())
+	slog.Info("Server closed", "error", listenAndServe(&server))
 }
